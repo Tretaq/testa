1448aa491413ba4820cec767389cd38d57d3ccaa
Treta <jakub.jamrozyy@gmail.com>
2025-10-16 00:06:58 +0200

    added all of map graphics now just scripts

--- player.gd
+++ player.gd
@@ -1,7 +1,20 @@
 extends CharacterBody2D
-@onready var animaterd_sprite = $AnimatedSprite2D
-func _physics_process(delta):
-    var direction = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
-    velocity = direction * 100
 
-    move_and_slide()
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	var direction = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
+	velocity = direction * 100
+	var direction_horizontal = Input.get_axis("LEFT", "RIGHT")
+
+	if direction_horizontal > 0:
+		animated_sprite.flip_h = false
+	elif direction_horizontal < 0:
+		animated_sprite.flip_h = true
+
+	if direction == Vector2.ZERO:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("move")
+
+	move_and_slide()
