aabb7caecb63ddda9dd523da545eca662bf61e29
Treta <jakub.jamrozyy@gmail.com>
2025-10-15 23:29:21 +0200

    added animations

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
-
+@onready var animaterd_sprite = $AnimatedSprite2D
 func _physics_process(delta):
     var direction = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
-    velocity = direction * 600
+    velocity = direction * 100
+
     move_and_slide()
