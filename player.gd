60206649c1ad9e8b6d0039b31d7eac43c04535c3
Treta <jakub.jamrozyy@gmail.com>
2025-10-19 01:53:35 +0200

    added player looking after the mouse

--- player.gd
+++ player.gd
@@ -1,20 +1,28 @@
 extends CharacterBody2D
 
+ 
+const MAX_SPEED: int = 80
+const ACCELERATION: int = 1400
+const FRICTION: int = 1000
+ 
+@onready var sprite = $AnimatedSprite2D
 @onready var animated_sprite = $AnimatedSprite2D
-
-func _physics_process(delta):
-	var direction = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
-	velocity = direction * 100
-	var direction_horizontal = Input.get_axis("LEFT", "RIGHT")
-
-	if direction_horizontal > 0:
-		animated_sprite.flip_h = false
-	elif direction_horizontal < 0:
-		animated_sprite.flip_h = true
-
-	if direction == Vector2.ZERO:
-		animated_sprite.play("idle")
+ 
+ 
+func _process(delta: float) -> void:
+	if get_global_mouse_position().x < global_position.x:
+		sprite.flip_h = true
 	else:
+		sprite.flip_h = false
+ 
+	var direction = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
+	if direction:
+		velocity.x = move_toward(velocity.x, MAX_SPEED * direction.x, ACCELERATION * delta)
+		velocity.y = move_toward(velocity.y, MAX_SPEED * direction.y, ACCELERATION * delta)
 		animated_sprite.play("move")
-
+	else:
+		velocity.x = move_toward(velocity.x, MAX_SPEED * direction.x, FRICTION * delta)
+		velocity.y = move_toward(velocity.y, MAX_SPEED * direction.y, FRICTION * delta)
+		animated_sprite.play("idle")
+	
 	move_and_slide()
