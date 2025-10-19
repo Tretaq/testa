be029d6dd6bd3310a62553e5fcab96f615eef7b6
Treta <jakub.jamrozyy@gmail.com>
2025-10-19 17:07:59 +0200

    PATH TRAVELING WOWO

--- bullet/bullet.gd
+++ bullet/bullet.gd
@@ -8,3 +8,7 @@ func _process(delta: float) -> void:
 
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
     queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+    queue_free()
--- mob/mob.gd
+++ mob/mob.gd
@@ -1,5 +1,17 @@
 extends CharacterBody2D
 
+const SPEED = 10
+
+@export var point: Area2D
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+
+func _physics_process(delta: float):
+    var dir = to_local(nav_agent.get_next_path_position()).normalized()
+    velocity = dir * SPEED
+    move_and_slide()
+
+func makepath() -> void:
+    nav_agent.target_position = point.global_position
 
 func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+    makepath()
